c33c879478d6b7c7dae43c27c4e72664317a3b27
Dimitrios Tsakos <s0578980@htw-berlin.de>
2024-05-26 17:06:45 +0200

    Repository restored

---
+++ src/views/TestPokerTableView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import TestPokerTable from "@/components/TestPokerTable.vue";
+</script>
+
+<template>
+  <main>
+    <div class="table">
+      <TestPokerTable />
+    </div>
+  </main>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
